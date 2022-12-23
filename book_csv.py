172370e8e80f7773eaac422616e947f08c396ada
Your Namecherguim <cherguim@gmail.com>
2022-12-23 16:46:41 +0100

    first commit

--- book_csv.py
+++ book_csv.py
@@ -3,7 +3,7 @@ import csv
 
 def book_csv (csv_file, Book_fields, Book_rows):
     with open(csv_file, 'w') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter='|')
+        csvwriter = csv.writer(csvfile, delimiter=',')
         csvwriter.writerow(Book_fields) 
         csvwriter.writerow(Book_rows)
         
